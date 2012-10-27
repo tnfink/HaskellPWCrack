05ecc80a7369286cc2334ee25d9a629994b48aea
Torsten Fink <torsten.fink@akquinet.de>
2012-10-27 20:03:31 +0200

    added parsing of command line parameters

--- HaskellPWCrack/src/main/Main.hs
+++ HaskellPWCrack/src/main/Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
+import System.Environment
+import System.Console.GetOpt
+
+data ParsedOption = Help
+                  | KeyChain String
+  deriving Eq
+
+suppportedOptions :: [OptDescr ParsedOption]
+suppportedOptions =
+  [ Option ['h'] ["help"] (NoArg Help)
+           "show this help message"
+  , Option ['k'] ["macKeyChain"] (ReqArg (\s -> KeyChain s) "path")
+           "attack a Mac OS X keychain located at path"
+  ]
+
+attackKeyChain keyChainPath = undefined
+
+
 main::IO()
-main = undefined
+main = do
+  argv <- getArgs
+  case (getOpt Permute suppportedOptions argv) of
+    ([Help],[],[])                    -> printHelp
+    ([KeyChain keyChainPath],[],[])   -> attackKeyChain keyChainPath
+    (_,_,_)                           ->
+         do putStrLn "Cannot process your configuration, sorry.\n"
+            printHelp
+  where
+    printHelp = putStrLn $ usageInfo "Usage: pwcrack [option]" suppportedOptions

9629b7a126612ca13006b4f00cd68afca46ecc75
Torsten Fink <torsten.fink@akquinet.de>
2012-10-27 21:40:14 +0200

    integration tests fails as wanted

--- HaskellPWCrack/src/test/IntegrationTests/MacOsXKeyChainAttack/Main.hs
+++ HaskellPWCrack/src/test/IntegrationTests/MacOsXKeyChainAttack/Main.hs
@@ -1,5 +1,17 @@
 module Main where
 
+import System.Process
+import System.Exit
+
 main::IO()
-main = print "to be done"
+main = do
+  (exitCode, _, _) <- readProcessWithExitCode pathToPwCrack arguments testStandardInput
+  case exitCode of
+    ExitSuccess      -> exitSuccess
+    ExitFailure _ -> exitFailure
+
+ where
+  pathToPwCrack = "../pwcrack/pwcrack"
+  arguments = ["-k","keychain"]
+  testStandardInput = ""
 

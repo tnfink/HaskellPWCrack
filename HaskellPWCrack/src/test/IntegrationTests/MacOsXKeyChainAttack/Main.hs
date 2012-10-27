ea13d55987d06a3b23f31da4445c2248c40fd3a0
Torsten Fink <torsten.fink@akquinet.de>
2012-10-27 21:55:42 +0200

    test now calls the application with the correct test data

--- HaskellPWCrack/src/test/IntegrationTests/MacOsXKeyChainAttack/Main.hs
+++ HaskellPWCrack/src/test/IntegrationTests/MacOsXKeyChainAttack/Main.hs
@@ -5,13 +5,18 @@ import System.Exit
 
 main::IO()
 main = do
-  (exitCode, _, _) <- readProcessWithExitCode pathToPwCrack arguments testStandardInput
+  (exitCode, stdout , stderr) <- readProcessWithExitCode pathToPwCrack arguments testStandardInput
   case exitCode of
     ExitSuccess      -> exitSuccess
-    ExitFailure _ -> exitFailure
+    ExitFailure code -> do
+      putStrLn $ "Exit-Code:" ++ (show code)
+      putStrLn $ "stdout:\n" ++ stdout
+      putStrLn $ "stderr:\n" ++ stderr
+      exitFailure
 
  where
-  pathToPwCrack = "../pwcrack/pwcrack"
-  arguments = ["-k","keychain"]
+  pathToPwCrack = "dist/build/pwcrack/pwcrack"
+  pathToKeyChain = "src/test/IntegrationTests/MacOsXKeyChainAttack/testKeyChain.keychain"
+  arguments = ["-k", pathToKeyChain]
   testStandardInput = ""
 
